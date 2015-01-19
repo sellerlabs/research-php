cca9980c7a0803ee5cd919db31d9f6cd2e651c39
Benjamin Kovach <bkovach5@uga.edu>
2015-01-19 12:04:11 -0500

    Add basic OffersResponse tests

--- test/SellerLabs/NodeMws/Responses/OffersResponseTest.php
+++ test/SellerLabs/NodeMws/Responses/OffersResponseTest.php
@@ -1,4 +1,6 @@
 <?php
+use SellerLabs\NodeMws\Responses\OffersResponse;
+
 /**
  * Created by PhpStorm.
  * User: Ben
@@ -6,6 +8,49 @@
  * Time: 11:49 AM
  */
 
-class OffersResponseTest {
+class OffersResponseTest extends PHPUnit_Framework_TestCase
+{
+    protected $offersResponse;
+    protected $responseInterfaceMock;
 
+    public function setUp()
+    {
+        $responseInterfaceMock = Mockery::mock('GuzzleHttp\Message\ResponseInterface');
+
+        $this->offersResponse = file_get_contents(dirname(__FILE__) . '/Resources/OffersResponse.json');
+
+        $responseInterfaceMock->
+        shouldReceive('getBody')
+            ->andReturn($this->offersResponse);
+
+        $this->responseInterfaceMock = $responseInterfaceMock;
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testConstruct()
+    {
+        $offers = new OffersResponse($this->responseInterfaceMock);
+
+        foreach ($offers->getFbaNewOffers() as $offer) {
+            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
+        }
+
+        foreach ($offers->getFbaUsedOffers() as $offer) {
+            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
+        }
+
+        foreach ($offers->getMerchantNewOffers() as $offer) {
+            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
+        }
+
+        foreach ($offers->getMerchantUsedOffers() as $offer) {
+            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
+        }
+
+        $this->assertEquals('B0097BEE9Q', $offers->getProductIdCode());
+    }
 }
