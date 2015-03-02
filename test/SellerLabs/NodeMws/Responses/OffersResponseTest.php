faec75995e721e99df066206148b08588c82020d
Eduardo Trujillo <ed@chromabits.com>
2015-03-02 16:56:29 -0500

    Add test namespace to autoloader

--- test/SellerLabs/NodeMws/Responses/OffersResponseTest.php
+++ test/SellerLabs/NodeMws/Responses/OffersResponseTest.php
@@ -1,16 +1,41 @@
 <?php
+
+namespace Tests\SellerLabs\NodeMws;
+
+use GuzzleHttp\Message\ResponseInterface;
+use Mockery;
+use PHPUnit_Framework_TestCase;
 use SellerLabs\NodeMws\Responses\OffersResponse;
 
+/**
+ * Class OffersResponseTest
+ *
+ * @author Benjamin Kovach <benjamin@roundsphere.com>
+ * @package Tests\SellerLabs\NodeMws
+ */
 class OffersResponseTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Response
+     *
+     * @var OffersResponse
+     */
     protected $offersResponse;
+
+    /**
+     * @var ResponseInterface
+     */
     protected $responseInterfaceMock;
 
     public function setUp()
     {
-        $responseInterfaceMock = Mockery::mock('GuzzleHttp\Message\ResponseInterface');
+        $responseInterfaceMock = Mockery::mock(
+            'GuzzleHttp\Message\ResponseInterface'
+        );
 
-        $this->offersResponse = file_get_contents(dirname(__FILE__) . '/Resources/OffersResponse.json');
+        $this->offersResponse = file_get_contents(
+            dirname(__FILE__) . '/Resources/OffersResponse.json'
+        );
 
         $responseInterfaceMock->
         shouldReceive('getBody')
@@ -28,20 +53,18 @@ class OffersResponseTest extends PHPUnit_Framework_TestCase
     {
         $offers = new OffersResponse($this->responseInterfaceMock);
 
-        foreach ($offers->getFbaNewOffers() as $offer) {
-            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
-        }
+        $results = array_merge(
+            $offers->getFbaNewOffers(),
+            $offers->getFbaUsedOffers(),
+            $offers->getMerchantNewOffers(),
+            $offers->getMerchantUsedOffers()
+        );
 
-        foreach ($offers->getFbaUsedOffers() as $offer) {
-            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
-        }
-
-        foreach ($offers->getMerchantNewOffers() as $offer) {
-            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
-        }
-
-        foreach ($offers->getMerchantUsedOffers() as $offer) {
-            $this->assertInstanceOf('\SellerLabs\NodeMws\Entities\Offer', $offer);
+        foreach ($results as $offer) {
+            $this->assertInstanceOf(
+                '\SellerLabs\NodeMws\Entities\Offer',
+                $offer
+            );
         }
 
         $this->assertEquals('B0097BEE9Q', $offers->getProductIdCode());
